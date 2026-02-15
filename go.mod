71c1dd46b8b1cc61bdda21c21f4c34be3c2eb5ce
gram <git@orsinium.dev>
2026-02-15 16:35:32 +0100

    draw bg

--- go.mod
+++ go.mod
@@ -1,7 +1,9 @@
 module remover
 
-go 1.25.0
+go 1.26.0
 
 require github.com/firefly-zero/firefly-go v0.10.0
 
-require github.com/orsinium-labs/tinymath v1.0.0 // indirect
+require github.com/orsinium-labs/tinymath v1.1.0 // indirect
+
+replace github.com/firefly-zero/firefly-go => ../firefly-go
--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "github.com/firefly-zero/firefly-go/firefly"
+import (
+	"strings"
+
+	"github.com/firefly-zero/firefly-go/firefly"
+)
+
+type State struct {
+	settings firefly.Settings
+	authorID string
+	appID    string
+	font     firefly.Font
+}
+
+var state State
 
 func init() {
 	firefly.Boot = boot
@@ -9,7 +22,16 @@ func init() {
 }
 
 func boot() {
-	// ...
+	me := firefly.GetMe()
+	state.settings = firefly.GetSettings(me)
+	state.font = firefly.LoadFont("font", nil)
+
+	target := firefly.LoadFile("target", nil)
+	if target.Raw != nil {
+		authorID, appID, _ := strings.Cut(string(target.Raw), ".")
+		state.authorID = authorID
+		state.appID = appID
+	}
 }
 
 func update() {
@@ -18,13 +40,46 @@ func update() {
 
 func render() {
 	firefly.ClearScreen(firefly.ColorWhite)
-	firefly.DrawTriangle(
-		firefly.Point{X: 60, Y: 10},
-		firefly.Point{X: 40, Y: 40},
-		firefly.Point{X: 80, Y: 40},
+	drawBackgroundGrid()
+	drawBackgroundBox()
+}
+
+func drawBackgroundGrid() {
+	const cellSize = 10
+	firefly.ClearScreen(firefly.ColorWhite)
+	lineStyle := firefly.L(firefly.ColorLightGray, 1)
+	for x := cellSize; x < firefly.Width; x += cellSize {
+		firefly.DrawLine(
+			firefly.P(x, 0),
+			firefly.P(x, firefly.Height),
+			lineStyle,
+		)
+	}
+	for y := cellSize; y < firefly.Height; y += cellSize {
+		firefly.DrawLine(
+			firefly.P(0, y),
+			firefly.P(firefly.Width, y),
+			lineStyle,
+		)
+	}
+}
+
+func drawBackgroundBox() {
+	const margin = 15
+	size := firefly.S(firefly.Width-margin*2, firefly.Height-margin*2)
+	firefly.DrawRoundedRect(
+		firefly.P(margin+1, margin+1),
+		size,
+		firefly.S(4, 4),
+		firefly.Solid(firefly.ColorBlack),
+	)
+	firefly.DrawRoundedRect(
+		firefly.P(margin, margin),
+		size,
+		firefly.S(4, 4),
 		firefly.Style{
-			FillColor:   firefly.ColorDarkBlue,
-			StrokeColor: firefly.ColorBlue,
+			FillColor:   firefly.ColorWhite,
+			StrokeColor: firefly.ColorBlack,
 			StrokeWidth: 1,
 		},
 	)
